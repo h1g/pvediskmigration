d12b39399916455d3602b90e56cb2baf0829429a
h1g <g.novicov@gmail.com>
2016-07-11 14:44:26 +0300

    Create move_disk.sh

---
+++ move_disk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+src_storage="src-stor"
+dst_storage="dst-stor"
+nodelist=`pvesh get /nodes/|grep "id"|sed 's/",//g'|sed 's/"//g'|awk '{print $3}'|sed 's/node/\/nodes/g'|grep -v sc10`
+for i in ${nodelist}
+do
+echo ${i}/qemu/
+for j in `pvesh get ${i}/qemu/|grep vmid|awk '{print $3}'`
+do
+echo ${i} ${j}.
+for k in `pvesh get ${i}/qemu/${j}/config |grep ${src_storage}":"|awk '{print $1}'|sed 's/"//g'`
+do
+pvesh create  ${i}/qemu/${j}/move_disk -disk ${k} -storage ${dst_storage} -delete 1
+done
+done
+done
+
