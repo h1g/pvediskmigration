ef492df607b8507ec7f1b95318021e9193e5bd6e
h1g <g.novicov@gmail.com>
2017-01-06 14:24:08 +0200

    add migration speed limit

--- move_disk.sh
+++ move_disk.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -m
 src_storage="src-stor"
 dst_storage="dst-stor"
+#migration speed in MB (0 - no limits)
+migration_speed=0
 nodelist=`pvesh get /nodes/|grep "id"|sed 's/",//g'|sed 's/"//g'|awk '{print $3}'|sed 's/node/\/nodes/g'|sort`
 for i in ${nodelist}
 do
@@ -10,8 +13,9 @@ do
 echo ${i} ${j}.
 for k in `pvesh get ${i}/qemu/${j}/config |grep ${src_storage}":"|awk '{print $1}'|sed 's/"//g'`
 do
-pvesh create  ${i}/qemu/${j}/move_disk -disk ${k} -storage ${dst_storage} -delete 1
+pvesh create  ${i}/qemu/${j}/move_disk -disk ${k} -storage ${dst_storage} -delete 1 &
+if [ ${migration_speed} -ne 0 ] ; then sleep 2 && pvesh create  ${i}/qemu/${j}/monitor -command "block_job_set_speed drive-${k} ${migration_speed}" ; fi
+fg %1
 done
 done
 done
-
